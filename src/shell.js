bf4a0853e37863e90ae368328d1764a00907a58d
Vinícius Saueia <vsaueia@gmail.com>
2016-09-18 18:44:44 -0400

    view port example

--- src/shell.js
+++ src/shell.js
@@ -6,10 +6,23 @@ export class Shell {
 		this.router = router;
 		config.title = "Palmeiras SEP";
 		config.map([
-				{ route: ['', 'events'], moduleId: 'events/events', name: 'Events', title: 'Events', nav: true },
-				{ route: 'jobs', moduleId: 'jobs/jobs', name: 'Jobs', title: 'Jobs', nav: true },
-				{ route: 'discussion', moduleId: 'discussion/discussion',title: 'Discussion', nav: true },
-				{ route: 'eventDetail/:eventId', moduleId: 'events/eventDetail', name: 'eventDetail'}
+				{ route: ['', 'events'],
+					viewPorts:{
+							mainContent: {moduleId: 'events/events'},
+							sideBar:{moduleId: 'sideBar/sponsors'}
+					}, name: 'Events', title: 'Events', nav: true },
+				{ route: 'jobs', viewPorts:{
+						mainContent: {moduleId: 'jobs/jobs'},
+						sideBar:{moduleId: 'sideBar/sponsors'}
+				}, name: 'Jobs', title: 'Jobs', nav: true },
+				{ route: 'discussion', viewPorts:{
+						mainContent: {moduleId: 'discussion/discussion'},
+						sideBar:{moduleId: 'sideBar/ads'}
+				},title: 'Discussion', nav: true },
+				{ route: 'eventDetail/:eventId',viewPorts:{
+						mainContent: {moduleId: 'events/eventDetail'},
+						sideBar:{moduleId: 'sideBar/ads'}
+				} , name: 'eventDetail'}
 			]);
 	}
 }
