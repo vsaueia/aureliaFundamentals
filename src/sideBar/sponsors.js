52f6aab35c368d8a2cb6f3633c96a4dd245c9990
Vinícius Saueia <vsaueia@gmail.com>
2016-09-21 09:15:23 -0400

    computedFrom

--- src/sideBar/sponsors.js
+++ src/sideBar/sponsors.js
@@ -1,14 +1,27 @@
-export class Sponsors {
-	constructor(){
-		this.message = "Sponsors";
-		setTimeout(() => this.message = "Changed after binding", 3000);
-		this.mapCollection = new window.Map();
-		this.mapCollection.set('a', 'Alpha');
-		this.mapCollection.set('b', 'Beta');
-		this.mapCollection.set('c', 'Charlie');
-		this.mapCollection.set('d', 'Delta');	}
+import {computedFrom} from 'aurelia-framework';
 
-	doSomething(foo) {
-		console.log(foo);
-	}
+export class Sponsors {
+    constructor() {
+        this.message = "Sponsors";
+        setTimeout(() => this.message = "Changed after binding", 3000);
+        this.mapCollection = new window.Map();
+        this.mapCollection.set('a', 'Alpha');
+        this.mapCollection.set('b', 'Beta');
+        this.mapCollection.set('c', 'Charlie');
+        this.mapCollection.set('d', 'Delta');
+        this.person = new Person();
+    }
+
+    doSomething(foo) {
+        console.log(foo);
+    }
+}
+
+class Person {
+    firstName: "Vinicius";
+    lastName: "Saueia";
+	@computedFrom("firstName", "lastName");
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    }
 }
